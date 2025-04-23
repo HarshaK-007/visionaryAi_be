4aea4778d6cf0b4a563f4129c59cc52cf202b593
HarshaK-007 <harshakancharana2002@gmail.com>
2025-04-24 00:00:32 +0530

    commented if __name__ == '__main__' in main.py file

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ async def health():
 
 app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev")) 
+# if __name__ == '__main__':
+    # uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev")) 
